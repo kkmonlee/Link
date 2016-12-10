1f36ba0037efe8119531a0ee2a79f241f46ffff8
kkmonlee <kkmonlee@gmail.com>
2016-12-10 19:07:11 +0000

    Wrote algorithm for core functionality

--- src/core.js
+++ src/core.js
@@ -1,3 +1,4 @@
+var canvas;
 /**
  * Created by aa on 10 December 2016.
  */
@@ -10,16 +11,16 @@ dotSize = Math.min(canvas.width, canvas.height) / 7;
 xs = canvas.width / 2 - dotSize * 3 + dotSize / 2;
 ys = canvas.height / 2 - dotSize * 3;
 
-colours = ['#F52616', '#9317A8', '#188FEC', '#4BCB50', '#FF8800'];
+colors = ['#F52616', '#9317A8', '#188FEC', '#4BCB50', '#FF8800'];
 
 selected = [];
 isSelecting = false;;
 mouseX = 0;
 mouseY = 0;
-squareColour = null;
+squareColor = null;
 
 score = 0;
-time = 60;
+var time = 60;
 
 setInterval(function () {
     time -= 1;
@@ -28,7 +29,7 @@ setInterval(function () {
     if (time == 0) {
         selected = [];
         isSelecting = false;
-        squareColour = null
+        squareColor = null
     }
 }, 1000);
 
@@ -37,12 +38,12 @@ choice = function(arr) {
 };
 
 dots = [];
-var colour;
+var color;
 for (var x = 0; x < 6; x++) {
     for (var y = 0; y < 6; y++) {
-        colour = choice(colours);
+        color = choice(colors);
         dots.push({
-            colour: colour,
+            color: color,
             ty: ys + y * dotSize,
             x: xs + x * dotSize,
             y: ys + y * dotSize,
@@ -52,10 +53,196 @@ for (var x = 0; x < 6; x++) {
     }
 }
 
+render = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = dots.length - 1; i >= 0; i--) {
+        var a = dots[i];
+        var hasBelow = false;
+
+        for (var j = 0; j < dots.length; j++) {
+            var b = dots[j];
+            if (isBelow(a, b)) {
+                hasBelow = true;
+                break;
+            }
+        }
+        if (!hasBelow && a.r != 5) {
+            a.r += 1;
+            a.ty = ys + a.r * dotSize;
+        }
+
+        if (a.y != a.ty) {
+            dir = a.y > a.ty ? -1 : 1;
+            a.y += a.tt * dir;
+            a.tt *= a.bdown && !a.bup ? 0.5 : 1.5;
+
+            if (dir == 1 && a.y > a.ty) {
+                a.y = a.ty;
+            } else if (dir == -1 && a.y <= a.ty) {
+                a.y = a.ty;
+                if (a.bdown) {
+                    a.bdown = true;
+                }
+            }
+
+            if (!a.bdown && !a.bup && a.y == a.ty) {
+                a.bdown = true;
+                a.ty -= dotSize / 2;
+                a.tt = dotSize / 3.2;
+            } else if (a.bdown && !a.bup && a.y == a.ty) {
+                a.bup = true;
+                a.tt = dotSize / 30;
+                a.ty += dotSize / 2;
+            }
+        } else {
+            a.tt = dotSize / 30;
+            a.bdown = false;
+            a.bup = false;
+        }
+    }
+
+    for (var i = 0; i < dots.length; i++) {
+        dot = dots[i];
+        if (contains(selected, dot) || dot.color == squareColor) {
+            context.fillStyle = dot.color
+            context.fillRect(dot.x - dotSize / 3, dot.y - dotSize / 3, dotSize / 1.5, dotSize / 1.5);
+        }
+
+        context.fillStyle = dot.color;
+        context.fillRect(dot.x - dotSize / 4, dot.y - dotSize / 4, dotSize / 2, dotSize / 2);
+    }
+
+    if (selected.length && isSelecting) {
+        context.strokeStyle = selected[0].color;
+        context.lineJoin = 'round';
+        context.lineWidth = dotSize / 6;
+        context.beginPath();
+
+        context.moveTo(mouseX, mouseY);
+
+        for (var i = 0; i < selected.length; i++) {
+            var dot = selected[i];
+            context.lineTo(dot.x, dot.y);
+        }
+        context.stroke();
+    }
+
+    context.font = dotSize / 3 + 'px sans'
+    context.fillStyle = '#000';
+    context.fillText('Score: ' + score, xs + dotSize * 3, ys + dotSize * 6);
+    context.fillText('Time: ' + time, xs, ys + dotSize * 6);
+
+    requestAnimationFrame(render);
+};
+
+isBelow = function (a, b) {
+    return a.r + 1 == b.r && a.c == b.c;
+};
+
+collideDot = function (x, y, dot) {
+    return x > dot.x - dotSize / 2 &&
+            x < dot.x + dotSize / 2 &&
+            y > dot.y - dotSize / 2 &&
+            y < dot.y + dotSize / 2;
+};
+
+contains = function (arr, x) {
+    return arr.indexOf(x) != -1;
+};
+
+isNeighbour = function (a, b) {
+    return a.r + 1 == b.r && a.c == b.c ||
+            a.r - 1 == b.r && a.c == b.c ||
+            a.c + 1 == b.c && a.r == b.r ||
+            a.c - 1 == b.c && a.r == b.r;
+};
+
+window.onMouseDown = window.onTouchStart = function (e) {
+    if (time == 0) return;
+    isSelecting = true;
+};
+
+window.onMouseUp = window.onTouchEnd = function (e) {
+    isSelecting = false;
+    if (selected.length < 2) {
+        return selected = [];
+    }
+
+    if (squareColor) {
+        for (var i = 0; i < dots.length; i++) {
+            var dot = dots[i];
+            if (dot.color == squareColor) {
+                selected.push(dot);
+            }
+        }
+    }
+
+    var highestRow = 0;
+    for (var i = 0; i < selected.length; i++) {
+        var dot = selected[i];
+        highestRow = Math.max(dot.r, highestRow);
+    }
+
+    for (var i = 0; i < selected.length; i++) {
+        var dot = selected[i];
+        do {
+            var color = choice(colors);
+        } while (color == squareColor);
+
+        if (dot.r >= 0) {
+            score += 1;
+            dot.r -= highestRow + 1;
+            dot.y = ys + dot.r * dotSize;
+            dot.ty = ys + dot.r * dotSize;
+            dot.color = color;
+        }
+    }
+
+    squareColor = null;
+    selected = [];
+};
+
+window.onMouseMove = window.onTouchMove = function (e) {
+    if (e.clientX) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+    } else {
+        var t = e.changedTouches;
+        mouseX = t[0].pageX;
+        mouseY = t[0].pageY;
+        isSelecting = true;
+    }
+
+    if (isSelecting && time != 0) {
+        for (var i = 0; i < dots.length; i++) {
+            var dot = dots[i];
+            var notSame = selected.length && selected[0].color != dot.color;
+
+            if (notSame || selected.length && !isNeighbour(dot, selected[0])) {
+                continue;
+            }
+
+            if (collideDot(mouseX, mouseY, dot)) {
+                if (!contains(selected, dot)) {
+                    selected.unshift(dot);
+                } else if (selected[1] == dot) {
+                    selected.shift();
+                } else {
+                    selected.unshift(dot);
+                    squareColor = dot.color;
+                }
+            }
+        }
+    }
+};
+
+requestAnimationFrame(render);
+
 /**
  * TODO: make render function with dots.length
  * TODO: make mouseDown/mouseUp functions
- * TODO: make function to check the nearby colour
+ * TODO: make function to check the nearby color
  * TODO: make function to join the dots up
  * TODO: think that should be it for the core framework
  */

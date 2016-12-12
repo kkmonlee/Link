57a3761f3ad8bd4509929b1c71c39448caa2bc04
kkmonlee <kkmonlee@gmail.com>
2016-12-12 01:03:29 +0000

    Deleted everything.
    Wrote everything again

--- src/core.js
+++ src/core.js
@@ -1,241 +1,169 @@
-var canvas;
 /**
  * Created by aa on 10 December 2016.
  */
 
-context = c;
-W = a.width;
-H = a.height;
-dotSize = Math.min(W, H) / 7;
-xs = W / 2 - dotSize * 3 + dotSize / 2;
-ys = H / 2 - dotSize * 3;
-
-colors = ['#F52616', '#9317A8', '#188FEC', '#4BCB50', '#FF8800'];
-
-selected = [];
-isSelecting = false;;
-mouseX = 0;
-mouseY = 0;
-squareColor = null;
-
-score = 0;
-var time = 60;
-
-setInterval(function () {
-    time -= 1;
-    time = Math.max(time, 0);
-
-    if (time == 0) {
-        selected = [];
-        isSelecting = false;
-        squareColor = null
+with (Math) {
+    for (i in c) {
+        c[i[x = m = n = k = 0] + (i[6] || '')] = c[i]
     }
-}, 1000);
 
-choice = function(arr) {
-    return arr[Math.floor(Math.random() * arr.length)];
-};
-
-dots = [];
-var color;
-for (var x = 0; x < 6; x++) {
-    for (var y = 0; y < 6; y++) {
-        color = choice(colors);
-        dots.push({
-            color: color,
-            ty: ys + y * dotSize,
-            x: xs + x * dotSize,
-            y: ys + y * dotSize,
-            r: y,
-            c: x
-        })
+    for (W = a.width,
+        H = a.height,
+        f = ['#F52616', '#9317A8', '#188FEC', '#4BCB50', '#FF8800'],
+        d = min(W, H) / 7,
+        L = W / 2 - d * 3 + d / 2,
+        R = H / 2 - d * 3,
+        g = [],
+        h = [],
+        onmousedown = function() {
+            k = 1
+        }; x < 6; x++) {
+        for (y = 0; y < 6; y++) {
+            g.push({
+                o: f[random() * 5 | 0],
+                Y: R + y * d,
+                x: L + x * d,
+                y: R + y * d,
+                r: y,
+                c: x
+            })
+        }
     }
+
+    onmouseup = ontouchend = function() {
+        // Ignore single colours being entered in score
+        if (!h[1]) {
+            h = []
+        }
+
+        for (i in h) {
+            p = h[i];
+            if (p.r >= 0) {
+                m++;
+                p.r -= n + 1;
+                p.y = R + p.r * d;
+                p.o = f[random() * 5 | 0]
+            }
+        }
+
+        n = k = 0;
+        h = []
+    };
+
+    onmousemove = ontouchmove = function (e) {
+        // touch input
+        if (e.pageX) {
+            X = e.pageX;
+            Y = e.pageY
+        } else {
+            X = e.touches[0].pageX;
+            k = Y = e.touches[0].pageY
+        }
+
+        if (k) {
+            for (i in g) {
+                p = g[i];
+
+                // Skip if dot is not on the same colour or is not next to it
+                if (h[0] &&
+                    (h[0].o != p.o |
+                        abs(
+                            abs(h[0].r - p.r) - abs(h[0].c - p.c)
+                        ) != 1)) {
+                    continue
+                }
+
+                if (
+                    abs(X - p.x) < d / 2 &&
+                        abs(Y - p.y) < d / 2
+                ) {
+                    if (!~h.indexOf(p)) {
+                        h.unshift(p);
+                        n = max(p.r, n)
+                    } else if (h[1] == p) {
+                        h.shift()
+                    }
+                }
+            }
+        }
+    };
+
+    setInterval(function() {
+        // clear canvas
+        c.ce(0, 0, W, H);
+        c.lineWidth = d / 6;
+        c.lineJoin = 'round';
+
+        // physics
+        for (i in g) {
+            p = g[i];
+
+            // if p doesn't have a neighbour below, push it down
+            r = 0;
+            for (j in g) {
+                q = g[j];
+                if (p.r + 1 == q.r && p.c == q.c) {
+                    r = 1
+                }
+            }
+
+            if (!r && p.r != 5) {
+                p.r++;
+                p.Y = R + p.r * d
+            }
+
+            // animation for p to push down
+            if (p.y != p.Y) {
+                D = p.y > p.Y ? -1 : 1;
+                p.y += p.T * D;
+                p.T *= p.s && !p.t ? 0.5 : 1.5;
+
+                if (~D && p.y >= p.Y) {
+                    p.y = p.Y
+                } else if (!~D && p.y <= p.Y) {
+                    p.y = p.Y
+                }
+
+                if (!p.s && !p.t && p.y == p.Y) {
+                    p.s = 1;
+                    p.Y -= d / 3;
+                    p.T = d / 5
+                } else if (p.s && !p.t && p.y == p.Y) {
+                    p.t = 1;
+                    p.T = d / 15;
+                    p.Y += d / 3
+                }
+            } else {
+                p.T = d / 15;
+                p.t = p.s = 0
+            }
+
+            // paint canvas
+            c.strokeStyle = p.o;
+            if (~h.indexOf(p)) {
+                c.sR(p.x - d / 3, p.y - d / 3, d / 1.5, d / 1.5)
+            }
+            c.sR(p.x - d / 4, p.y - d / 4, d / 2, d / 2)
+        }
+
+        // m
+        c.font = d / 3 + 'px s';
+        c.fx('Z ' + m, L, R + d * 6);
+
+        // draw selection lines
+        if (h[0]) {
+            c.strokeStyle = h[0].o;
+            c.ba();
+            c.m(X, Y);
+            for (i in h) {
+                p = h[i];
+                c.l(p.x, p.y)
+            }
+            c.s()
+        }
+    }, 33)
 }
 
-render = function () {
-    context.clearRect(0, 0, W, H);
-
-    for (var i = dots.length - 1; i >= 0; i--) {
-        var a = dots[i];
-        var hasBelow = false;
-
-        for (var j = 0; j < dots.length; j++) {
-            var b = dots[j];
-            if (isBelow(a, b)) {
-                hasBelow = true;
-                break;
-            }
-        }
-        if (!hasBelow && a.r != 5) {
-            a.r += 1;
-            a.ty = ys + a.r * dotSize;
-        }
-
-        if (a.y != a.ty) {
-            dir = a.y > a.ty ? -1 : 1;
-            a.y += a.tt * dir;
-            a.tt *= a.bdown && !a.bup ? 0.5 : 1.5;
-
-            if (dir == 1 && a.y > a.ty) {
-                a.y = a.ty;
-            } else if (dir == -1 && a.y <= a.ty) {
-                a.y = a.ty;
-                if (a.bdown) {
-                    a.bdown = true;
-                }
-            }
-
-            if (!a.bdown && !a.bup && a.y == a.ty) {
-                a.bdown = true;
-                a.ty -= dotSize / 3;
-                a.tt = dotSize / 5;
-            } else if (a.bdown && !a.bup && a.y == a.ty) {
-                a.bup = true;
-                a.tt = dotSize / 15;
-                a.ty += dotSize / 3;
-            }
-        } else {
-            a.tt = dotSize / 15;
-            a.bdown = false;
-            a.bup = false;
-        }
-    }
-
-    for (var i = 0; i < dots.length; i++) {
-        dot = dots[i];
-        if (contains(selected, dot) || dot.color == squareColor) {
-            context.fillStyle = dot.color
-            context.fillRect(dot.x - dotSize / 3, dot.y - dotSize / 3, dotSize / 1.5, dotSize / 1.5);
-        }
-
-        context.fillStyle = dot.color;
-        context.fillRect(dot.x - dotSize / 4, dot.y - dotSize / 4, dotSize / 2, dotSize / 2);
-    }
-
-    if (selected.length && isSelecting) {
-        context.strokeStyle = selected[0].color;
-        context.lineJoin = 'round';
-        context.lineWidth = dotSize / 6;
-        context.beginPath();
-
-        context.moveTo(mouseX, mouseY);
-
-        for (var i = 0; i < selected.length; i++) {
-            var dot = selected[i];
-            context.lineTo(dot.x, dot.y);
-        }
-        context.stroke();
-    }
-
-    context.font = dotSize / 3 + 'px sans'
-    context.fillStyle = '#000';
-    context.fillText('Score: ' + score, xs + dotSize * 3, ys + dotSize * 6);
-    context.fillText('Time: ' + time, xs, ys + dotSize * 6);
-
-};
-
-isBelow = function (a, b) {
-    return a.r + 1 == b.r && a.c == b.c;
-};
-
-collideDot = function (x, y, dot) {
-    return x > dot.x - dotSize / 2 &&
-            x < dot.x + dotSize / 2 &&
-            y > dot.y - dotSize / 2 &&
-            y < dot.y + dotSize / 2;
-};
-
-contains = function (arr, x) {
-    return arr.indexOf(x) != -1;
-};
-
-isNeighbour = function (a, b) {
-    return a.r + 1 == b.r && a.c == b.c ||
-            a.r - 1 == b.r && a.c == b.c ||
-            a.c + 1 == b.c && a.r == b.r ||
-            a.c - 1 == b.c && a.r == b.r;
-};
-
-window.onMouseDown = window.onTouchStart = function (e) {
-    if (time == 0) return;
-    isSelecting = true;
-};
-
-window.onMouseUp = window.onTouchEnd = function (e) {
-    isSelecting = false;
-    if (selected.length < 2) {
-        return selected = [];
-    }
-
-    if (squareColor) {
-        for (var i = 0; i < dots.length; i++) {
-            var dot = dots[i];
-            if (dot.color == squareColor) {
-                selected.push(dot);
-            }
-        }
-    }
-
-    var highestRow = 0;
-    for (var i = 0; i < selected.length; i++) {
-        var dot = selected[i];
-        highestRow = Math.max(dot.r, highestRow);
-    }
-
-    for (var i = 0; i < selected.length; i++) {
-        var dot = selected[i];
-        do {
-            var color = choice(colors);
-        } while (color == squareColor);
-
-        if (dot.r >= 0) {
-            score += 1;
-            dot.r -= highestRow + 1;
-            dot.y = ys + dot.r * dotSize;
-            dot.ty = ys + dot.r * dotSize;
-            dot.color = color;
-        }
-    }
-
-    squareColor = null;
-    selected = [];
-};
-
-window.onMouseMove = window.onTouchMove = function (e) {
-    if (e.clientX) {
-        mouseX = e.pageX;
-        mouseY = e.pageY;
-    } else {
-        var t = e.changedTouches;
-        mouseX = t[0].pageX;
-        mouseY = t[0].pageY;
-        isSelecting = true;
-    }
-
-    if (isSelecting && time != 0) {
-        for (var i = 0; i < dots.length; i++) {
-            var dot = dots[i];
-            var notSame = selected.length && selected[0].color != dot.color;
-
-            if (notSame || selected.length && !isNeighbour(dot, selected[0])) {
-                continue;
-            }
-
-            if (collideDot(mouseX, mouseY, dot)) {
-                if (!contains(selected, dot)) {
-                    selected.unshift(dot);
-                } else if (selected[1] == dot) {
-                    selected.shift();
-                } else {
-                    selected.unshift(dot);
-                    squareColor = dot.color;
-                }
-            }
-        }
-    }
-};
-
-setInterval(render, 33);
 
 /**
  * TODO: make render function with dots.length
